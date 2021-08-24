a2bc508d6e4445975801ec40e4876c9b57eb2001
nascott <nascott94@outlook.com>
2021-08-24 16:21:21 -0600

    fixed file structure

---
+++ index.js
@@ -0,0 +1,15 @@
+//print to html page 
+const generateHTML = require();
+
+//node stuff
+const inquirer = require(inquirer);
+
+//tests
+const intern = require();
+const engineer = require();
+const manager = require();
+const employee = [];
+
+class Team {
+    constructor()
+}
